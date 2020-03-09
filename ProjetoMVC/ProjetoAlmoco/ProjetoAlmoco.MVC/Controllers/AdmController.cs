a2990f965a8c2ed77475c74a593c4c8a06510303
Laura <laura.ratis@gmail.com>
2020-03-09 17:11:41 -0300

    FRT003 - design de FRT003 implementado

--- ProjetoMVC/ProjetoAlmoco/ProjetoAlmoco.MVC/Controllers/AdmController.cs
+++ ProjetoMVC/ProjetoAlmoco/ProjetoAlmoco.MVC/Controllers/AdmController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using ProjetoAlmoco.Application.Categoria;
+using ProjetoAlmoco.Application.Categoria.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,9 +11,17 @@ namespace ProjetoAlmoco.MVC.Controllers
 {
     public class AdmController : Controller
     {
+        private readonly CategoriaApplication _categoriaApplication;
+
+        public AdmController()
+        {
+            _categoriaApplication = new CategoriaApplication();
+        }
         // GET: Adm
         public ActionResult Index()
         {
+            HttpResponseMessage response = _categoriaApplication.GetAllCategoria();
+            ViewBag.GetAllCategorys = response.Content.ReadAsAsync<List<CategoriaModel>>().Result;
             return View();
         }
     }
